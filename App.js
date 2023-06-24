62ccb3a10742902bc3fbe669d7a62f72e1e4c964
Marina Wang <marinawangmw@gmail.com>
2023-06-24 18:10:00 -0300

    Change text in app.js (#3)

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Home />
+      <Text>Add your own homepage!</Text>
       <StatusBar style="auto" />
     </View>
   );
