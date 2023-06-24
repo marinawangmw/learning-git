663bcfa47c3b85bd27c264fc7a047765e68ac599
Marina Wang <marinawangmw@gmail.com>
2023-06-24 17:39:53 -0300

    Add home page (#1)

---
+++ src/Screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Welcome to my world!</Text>
+        </View>
+    );
+};
+
+export default Home;
